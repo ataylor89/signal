a2321db4b1cfb755e26721676adf4cf23a90d92e
Andrew Taylor <rolyata47@gmail.com>
2025-08-08 17:59:30 -0500

    Changing printf messages

--- arm/sigcont.c
+++ arm/sigcont.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
     if (argc == 2) {
         int pid = atoi(argv[1]);
         sigcont(pid);
-        printf("Sent a SIGCONT signal to pid %d.\n", pid);
+        printf("Sent a resume signal to pid %d.\n", pid);
     }
     else {
         printf("Usage: sigcont <pid>\n");
--- arm/sigstop.c
+++ arm/sigstop.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
     if (argc == 2) {
         int pid = atoi(argv[1]);
         sigstop(pid);
-        printf("Sent a SIGSTOP signal to pid %d.\n", pid);
+        printf("Sent a stop signal to pid %d.\n", pid);
     }
     else {
         printf("Usage: sigstop <pid>\n");
