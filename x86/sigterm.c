ca57259942288580f7f2399edfc06a874b1d25f6
Andrew Taylor <rolyata47@gmail.com>
2025-08-09 09:14:40 -0500

    Updating printf statements

--- x86/sigterm.c
+++ x86/sigterm.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
     if (argc == 2) {
         int pid = atoi(argv[1]);
         sigterm(pid);
-        printf("Sent a SIGTERM signal to pid %d.\n", pid);
+        printf("Sent a quit signal to pid %d.\n", pid);
     }
     else {
         printf("Usage: sigterm <pid>\n");
